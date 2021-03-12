a50c54cb05cc09ebd8186e9b4a9da939ce2e766d
krys <kjaniak07@gmail.com>
2021-03-12 20:57:30 +0100

    AssertionsDemo

---
+++ src/test/java/AssertionsDemo.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class AssertionsDemo {
+
+    private final Calculator calculator = new Calculator();
+
+    private final Person person = new Person("Jane", "Doe");
+
+    @Test
+    void standardAssertions() {
+        assertEquals(2, calculator.add(1, 1));
+        assertEquals(4, calculator.multiply(2,2),
+                "The optional failure message is now the last parameter");
+        assertTrue('a' < 'b', () -> "Assertion messages can be lazily evaluated --"
+                                    + "to avoid constructing complex messages unnecessarily.");
+    }
+
+
+
+}
